bc6b53f22f7a33aa11c9b24a9ac978141298a0f6
Siliprandi Diego <siliprandi.diego@osdbsesto>
2022-02-17 11:13:16 +0100

    second commit

--- backend/index.js
+++ backend/index.js
@@ -5,4 +5,8 @@ var host = "localhost";
 var port = 3000;
 apiServer.listen(port, host, ()=>{
     console.log("Server running: http://%s:%d/", host, port);
-})
+});
+
+apiServer.get("/api/login", (req, res) => {
+    console.log("recived:", req.query.mail, req.query.password);
+});
