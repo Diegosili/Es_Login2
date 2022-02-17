bc284cfe2b77166b1f77c2a3cfb41d41924daef2
Siliprandi Diego <siliprandi.diego@osdbsesto>
2022-02-17 11:30:25 +0100

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
-var express = require("espress");
+var express = require("express");
 var apiServer = express();
+apiServer.use(cors());
 
 var host = "localhost";
 var port = 3000;
@@ -9,4 +10,9 @@ apiServer.listen(port, host, ()=>{
 
 apiServer.get("/api/login", (req, res) => {
     console.log("recived:", req.query.mail, req.query.password);
+    if(req.query.mail==="mattia" && req.query.password==="pogliani"){
+        res.sendStatus(200);
+    } else{
+        res.status(400);
+    }
 });
